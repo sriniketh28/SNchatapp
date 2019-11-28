8a67777b36a00d09700c54d698c567bc81d5a5fa
sriniketh28 <39892279+sriniketh28@users.noreply.github.com>
2019-11-29 00:18:21 +0530

    Add files via upload

---
+++ chat/README.md
@@ -0,0 +1,18 @@
+# SNchatapp
+SNchatapp is a simple Webapp for chatting using React.js, Node.js, Socket.io and Express.js .
+
+## Steps to install
+
+1.Download the zip file and extract it.
+
+2.Open two terminals and cd to client in one terminal and cd to server in one terminal.
+
+3.Type ``` npm i ``` in both the terminals.
+
+4.Then type ``` npm start ``` in server terminal and then in client terminal.
+
+## SNchatapp
+
+**SNchatapp is live.**
+
+Click on this [SNchatapp](https://5ddfbbc307c9684c143dabc5--distracted-dijkstra-a1525c.netlify.com) to see it live.
---
+++ chat/client/build/precache-manifest.da67c76f506dbbaa4c1f79e6452ff636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ead6ba516d30f1b2fa888dbb05f6f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1f8235443aacf37da2c",
+    "url": "/static/css/main.e6ef0787.chunk.css"
+  },
+  {
+    "revision": "70996eec40d198b10582",
+    "url": "/static/js/2.be09fdb5.chunk.js"
+  },
+  {
+    "revision": "d1f8235443aacf37da2c",
+    "url": "/static/js/main.ef89504e.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
